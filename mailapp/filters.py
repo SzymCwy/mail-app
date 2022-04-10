2f75b6260a2cfce2a1eaf0e7e6a4e459d01d0411
SzymCwy <szymon.cwynar@icloud.com>
2022-04-11 01:45:22 +0200

    Added filter

--- mailapp/filters.py
+++ mailapp/filters.py
@@ -6,7 +6,8 @@ from .models import Email
 class EmailFilter(filters.FilterSet):
     date_from = filters.DateFilter(field_name='date', lookup_expr='gte', widget=forms.DateInput(attrs={'type': 'date'}))
     date_till = filters.DateFilter(field_name='date', lookup_expr='lte', widget=forms.DateInput(attrs={'type': 'date'}))
+    sent = filters.BooleanFilter(field_name='sent_date', lookup_expr='isnull')
 
     class Meta:
         model = Email
-        fields = ['date']
+        fields = ['sent']
