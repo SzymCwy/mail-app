00640bf801f4a8e8c7d389e175e19db8800e5f38
SzymCwy <szymon.cwynar@icloud.com>
2022-04-11 23:43:28 +0200

    debug = false

--- mailproject/settings.py
+++ mailproject/settings.py
@@ -37,7 +37,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
